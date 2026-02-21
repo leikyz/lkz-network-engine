494369162e1c00b5c0b88ac0784b1ecbdbfb9382
leikyz <leikyz17@outlook.com>
2026-02-21 19:31:33 +0100

    fix: re add entity dead message

--- Server/src/Protocol/Message/Entity/EntityDeadMessage.cpp
+++ Server/src/Protocol/Message/Entity/EntityDeadMessage.cpp
@@ -4,6 +4,7 @@
 #include <LKZ/Core/ECS/Manager/EntityManager.h>
 #include <LKZ/Core/ECS/Component/Component.h>
 #include <LKZ/Core/ECS/Manager/EntityManager.h>
+#include <iostream>
 
 EntityDeadMessage::EntityDeadMessage() {};
 
@@ -21,6 +22,7 @@ std::vector<uint8_t>& EntityDeadMessage::serialize(Serializer& serializer) const
 {
     serializer.reset();
 
+    serializer.writeUInt16(5);
     serializer.writeByte(ID);
     serializer.writeUInt16(entityId);
 
@@ -35,27 +37,28 @@ void EntityDeadMessage::deserialize(Deserializer& deserializer)
 
 void EntityDeadMessage::process(const sockaddr_in& senderAddr, SOCKET tcpSocket)
 {
-    /*Client* client = ClientManager::getClientByAddress(senderAddr);
-    Session* session = SessionManager::GetSession(client->lobbyId);
+    Session* session = SessionManager::GetSessionBySocket(tcpSocket);
 
-    if (!client || !session)
+    if (!session)
         return;
 
     ComponentManager& components = ComponentManager::Instance();
 
-	WaveComponent* waveComponent = components.GetComponent<WaveComponent>(EntityManager::Instance().GetEntityById(lobby->gameWaveEntity, lobby));
+	/*WaveComponent* waveComponent = components.GetComponent<WaveComponent>(EntityManager::Instance().GetEntityById(lobby->gameWaveEntity, lobby));
 
-    waveComponent->zombiesAlive--;
+    waveComponent->zombiesAlive--;*/
 
 	EntityManager::Instance().DestroyEntity(entityId);
+
+    std::cout << "entity destroyed" << std::endl;
 
     Serializer serializer;
     serialize(serializer);
 
-    Engine::Instance().Server()->SendToMultiple(
-        session->clientsAddress,
-        serializer.getBuffer(),
-        getClassName()
-    ); */
+    for (const auto& player : session->players)
+    {
+        Engine::Instance().Server()->SendReliable(player.tcpSocket, serializer.getBuffer());
+
+    }
 
 }
